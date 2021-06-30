20bb7de7b32869164f3867f09e7c32c8f28c3218
Ibrahiem <ibrahimalgharraa@gmail.com>
2021-06-30 03:22:29 +0300

    Add Posts

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
+    protected $fillable = [
+        'id', 'counter', 'user_id','post_id','created_at','updated_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at','updated_at',
+    ];
     use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+##################### Begain Route for Posts ########################################
+Route::get('create', [PostController::class,'create']);
+Route::post('store', [PostController::class,'store'])->name('posts.store');
+##################### End Route for Posts ########################################
